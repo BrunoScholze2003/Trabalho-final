762ada2d7f37994c5cebdbdd9d586c6b162be876
BrunoScholze2003 <104472351+BrunoScholze2003@users.noreply.github.com>
2022-07-05 17:24:02 -0300

    Create Prova.java

---
+++ Prova.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Prova implements ProvaI{
+    HashMap<Integer,Estudante> estudantes = new HashMap<Integer,Estudante>();
+
+    private double nota;
+
+    Prova(double nota){
+        this.nota = nota;
+    }
+    
+    public void setNotaProva(double notaProva){
+        this.nota = notaProva;
+    }
+
+    public double getNota(){
+      return this.nota;
+    }
+}
+
+    
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Prova implements ProvaI{
+    HashMap<Integer,Estudante> estudantes = new HashMap<Integer,Estudante>();
+
+    private double nota;
+
+    Prova(double nota){
+        this.nota = nota;
+    }
+    
+    public void setNotaProva(double notaProva){
+        this.nota = notaProva;
+    }
+
+    public double getNota(){
+      return this.nota;
+    }
+}
+
+    
